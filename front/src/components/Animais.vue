15af9d724b4574e0aa68905a2efcce5e68e45269
laique santana <laiquesantana@hotmail.com>
2019-02-21 02:31:28 -0300

    criacao dos components , iniciais

---
+++ front/src/components/Animais.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                   <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
+
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+        mounted() {
+        
+        }
+    }
+</script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,42 @@
+
+require('./bootstrap');
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+import moment from 'moment';
+import VueRouter from 'vue-router'
+
+
+import Swal from 'sweetalert2'
+window.Swal = Swal;
+
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+})
+
+
+
+
+Vue.component('animais', require('./components/Animais.vue').default);
+Vue.component('rodape', require('./components/Rodape.vue').default);
+
+
+Vue.filter('upText',function(text){
+  return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate',function(created){
+  moment.locale('pt-BR');
+  return moment(created).format('LLLL');
+});
+
 
 new Vue({
   render: h => h(App),
