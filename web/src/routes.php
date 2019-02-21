fe31543dbeb5656b4ec69d7869ae932b73fee629
laique santana <laiquesantana@hotmail.com>
2019-02-20 22:47:34 -0300

    adicionando dependencias no composer json para routes

--- web/src/routes.php
+++ web/src/routes.php
@@ -3,6 +3,6 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 // Routes
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-   
+    return $this->renderer->render($response, 'index.phtml', $args);
        return $response->getBody()->write("Nome do Pet: " . $args['name']);
 });
